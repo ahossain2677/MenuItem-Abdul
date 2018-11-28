fedbb58cb556887655aae948aaab70b3991c3cf9
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-11-28 08:11:51 -0500

    Update Salad.java

--- src/Salad.java
+++ src/Salad.java
@@ -1,2 +1,13 @@
-public class Salad {
+public class Salad implements MenuItem
+{
+
+   private double Price;
+   private String Name;
+   
+   public Salad (double Price,String Name){
+    this.Name = Name;
+    this.Price= Price;
+  }
+  {
+
 }

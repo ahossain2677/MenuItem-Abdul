2d93ed96d9a48673809d47c241b8ee549397f0c0
ahossain2677 <43207698+ahossain2677@users.noreply.github.com>
2018-11-28 08:17:11 -0500

    Update Trio.java

--- src/Trio.java
+++ src/Trio.java
@@ -1,4 +1,13 @@
-public class Trio {
+public class Trio implements MenuItem {
+
+   private double Price;
+   private String Name;
+   
+   public Trio (double Price,String Name){
+    this.Name = Name;
+    this.Price= Price;
+  }
+  {
 
 
 }
